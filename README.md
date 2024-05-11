a5bc42d856706c0bfa8b6eb7004835cca3212c42
Cyrus28214 <1270302477@qq.com>
2024-05-11 23:52:23 +0800

    feat: 用tcl脚本管理Vivado工程

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # FPGA-project
+
+数逻期末project
+
+## git协作方法
+
+说明，为了使用git协作编辑Vivado项目并进行版本管理，使用tcl来创建和更新工程，使用方法：
+
+```powershell
+vivado -mode tcl -source create_project.tcl
+```
+
+每次在Vivado中创建新的资源时，请从Tcl Console中拷贝创建资源的命令，并在create_project.tcl中添加到相应位置。
+
+每次pull时，可以手动查看tcl脚本里的更改，并在Vivado中的Tcl Console中运行。
---
+++ create_project.tcl
@@ -0,0 +1,34 @@
+# Typical usage: vivado -mode tcl -source create_project.tcl
+
+$target_dir = "vivado"
+
+create_project -force $target_dir ./$target_dir -part xc7k160tffg676-2L
+
+# Add sources
+add_files -fileset sources_1 ./rtl
+add_files -fileset sim_1 ./sim
+add_files -fileset constrs_1 ./constraints
+import_files -force
+update_compile_order -fileset sources_1
+update_compile_order -fileset sim_1
+
+# Add IPs
+create_ip -name clk_wiz -vendor xilinx.com -library ip -version 6.0 -module_name clk_wiz_0
+set_property -dict [list \
+  CONFIG.CLKIN1_JITTER_PS {50.0} \
+  CONFIG.CLKOUT1_JITTER {238.195} \
+  CONFIG.CLKOUT1_PHASE_ERROR {205.969} \
+  CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {25.175} \
+  CONFIG.CLK_OUT1_PORT {clk_out} \
+  CONFIG.MMCM_CLKFBOUT_MULT_F {38.000} \
+  CONFIG.MMCM_CLKIN1_PERIOD {5.000} \
+  CONFIG.MMCM_CLKIN2_PERIOD {10.0} \
+  CONFIG.MMCM_CLKOUT0_DIVIDE_F {43.125} \
+  CONFIG.MMCM_DIVCLK_DIVIDE {7} \
+  CONFIG.PRIMARY_PORT {clk_in} \
+  CONFIG.PRIM_IN_FREQ {200.000} \
+  CONFIG.USE_LOCKED {false} \
+  CONFIG.USE_RESET {false} \
+] [get_ips clk_wiz_0]
+
+start_gui
