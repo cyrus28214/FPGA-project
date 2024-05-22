58eed09fb21b5af77a6a9fa72b4743572f41f099
Cyrus28214 <1270302477@qq.com>
2024-05-22 20:43:17 +0800

    feat: vga 相关

--- rtl/top.v
+++ rtl/top.v
@@ -1,22 +1,36 @@
-module top(CLK_200M_P, vga_hs, vga_vs, vga_red, vga_green, vga_blue);
-    input      CLK_200M_P;
-    output vga_hs, vga_vs;
-    output [3:0] vga_red, vga_green, vga_blue;
+module top (
+    input wire clk,  //100MHz
+    input wire RSTN,
+    output wire vga_hs,
+    output wire vga_vs,
+    output wire [3:0] vga_red,
+    output wire [3:0] vga_green,
+    output wire [3:0] vga_blue
+);
+  wire vga_clk;
+  wire [3:0] vga_rgb;
+  wire [11:0] pixel = 12'hF00;
+  wire pix_x;
+  wire pix_y;
 
-    wire vga_clk;
+  clk_div_vga clk_div_vga_inst (
+      .clk_in (clk),
+      .clk_out(vga_clk)
+  );
 
-    clk_wiz_0 clk_wiz_0_inst(
-       .clk_in(CLK_200M_P),
-       .clk_out(vga_clk)
-    );
+  vga_ctrl vga_ctrl_inst (
+      .clk(vga_clk),
+      .rstn(RSTN),
+      .pixel(pixel),
+      .pix_x(pix_x),
+      .pix_y(pix_y),
+      .hs(vga_hs),
+      .vs(vga_vs),
+      .rgb(vga_rgb)
+  );
 
-    vga vga_inst(
-        .clk(vga_clk),
-        .hs(vga_hs),
-        .vs(vga_vs),
-        .r(vga_red),
-        .g(vga_green),
-        .b(vga_blue)
-    );
+  assign vga_red   = vga_rgb[3:0];
+  assign vga_green = vga_rgb[7:4];
+  assign vga_blue  = vga_rgb[11:8];
 
 endmodule
--- rtl/vga_ctrl.v
+++ rtl/vga_ctrl.v
@@ -1,36 +1,54 @@
-module vga_ctrl(clk, hs, vs, r, g, b);
-    input            clk; //25.172MHz@60Hz
-    output reg       hs, vs;
-    output reg [3:0] r, g, b;
+module vga_ctrl (
+    input wire clk,  //25.172MHz@60Hz
+    input wire rstn,
+    input wire [11:0] pixel,
+    output wire [9:0] pix_x,  //有效时输出像素坐标
+    output wire [9:0] pix_y,  //无效时输出全一: 10'h3FF
+    output wire hs,
+    output wire vs,
+    output wire [11:0] rgb
+);
+  //Horizontal timing
+  parameter HS_sync = 96;  //Sync
+  parameter HS_left = HS_sync + 48;  //Back Porch + Left Border
+  parameter HS_video = HS_left + 640;  //Video
+  parameter HS_right = HS_video + 16;  //Front Porch + Right Border
+  parameter HS_total = 800;  //Total
 
-    //Horizontal timing
-    parameter HS_sync  = 96;     //Sync
-    parameter HS_left  = 48;     //Back Porch + Left Border
-    parameter HS_video = 640;    //Video
-    parameter HS_right = 16;     //Front Porch + Right Border
-    parameter HS_total = 800;    //Total
+  //Vertical timing
+  parameter VS_sync = 2;  //Sync
+  parameter VS_top = VS_sync + 33;  //Back Porch + Top Border
+  parameter VS_video = VS_top + 480;  //Video
+  parameter VS_bottom = VS_bottom + 10;  //Front Porch + Bottom Border
+  parameter VS_total = 525;  //Total
 
-    //Vertical timing
-    parameter VS_sync  = 2;      //Sync
-    parameter VS_top   = 33;     //Back Porch + Top Border
-    parameter VS_video = 480;    //Video
-    parameter VS_bottom= 10;     //Front Porch + Bottom Border
-    parameter VS_total = 525;    //Total
+  reg [9:0] hcnt, vcnt;
 
-    reg [9:0] hcnt,
-              vcnt;
-
-    always @ (posedge clk) begin
-
-        hcnt <= (hcnt == HS_total - 1) ? 0 : hcnt + 1;
-        vcnt <= (vcnt == VS_total - 1) ? 0 : vcnt + 1;
-
-        hs <= (hcnt < VS_sync) ? 1 : 0;
-        vs <= (vcnt < HS_sync) ? 1 : 0;
-
-        r <= 4'b1111;
-        g <= 4'b0000;
-        b <= 4'b0000;
+  always @(posedge clk or negedge rstn) begin
+    if (!rstn) begin
+      hcnt <= 0;
+      vcnt <= 0;
+    end else begin
+      hcnt <= (hcnt == HS_total - 1) ? 0 : hcnt + 1;
+      vcnt <= (vcnt == VS_total - 1) ? 0 : vcnt + 1;
     end
+  end
+
+  assign hs = (hcnt < VS_sync);
+  assign vs = (vcnt < HS_sync);
+
+  wire video_on = (HS_left <= hcnt)
+    && (hcnt < HS_video)
+    && (VS_top <= vcnt)
+    && (vcnt < VS_video); //有效信号
+
+  wire pixel_req = (HS_left - 1 <= hcnt)
+    && (hcnt < HS_video - 1)
+    && (VS_top - 1 <= vcnt)
+    && (vcnt < VS_video - 1); //请求像素信号，比video_on提前一个时钟
+
+  assign pix_x = (pixel_req) ? hcnt + 1 - HS_left : 10'h3FF;
+  assign pix_y = (pixel_req) ? vcnt + 1 - VS_top : 10'h3FF;
+  //请求的像素实际上是下一个时钟的，因此要+1
 
 endmodule
