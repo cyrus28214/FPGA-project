5179c62111694b2c6436f6b2895c0cd774744aee
Cyrus28214 <1270302477@qq.com>
2024-05-22 20:45:57 +0800

    chore

--- sim/clk_div_vga_tb.v
+++ sim/clk_div_vga_tb.v
@@ -1,11 +1,11 @@
 `timescale 1ns / 1ps
 
 // 进行模拟的时候请运行足够多的时间才能看到效果，可以点击上方的Run for 10 us按钮
-module clk_wiz_vga_tb;
+module clk_div_vga_tb;
   reg  clk_in = 0;
   wire clk_out;
 
-  clk_wiz_0 dut (
+  clk_div_vga dut (
       .clk_in (clk_in),
       .clk_out(clk_out)
   );
