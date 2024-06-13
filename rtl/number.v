a97a6a32a6c9d1bd78614c78921bf88afd6cf1e8
Cyrus28214 <1270302477@qq.com>
2024-06-13 22:17:29 +0800

    fix: 解决文字杂色

--- rtl/number.v
+++ rtl/number.v
@@ -1,4 +1,4 @@
-module number(
+module number (
     input wire clk,
     input wire rstn,
     input wire [15:0] floor,
@@ -8,70 +8,87 @@ module number(
     output wire [15:0] dst_data,
     output wire dst_wr
 );
+  //一个digit给2^11个时钟
+  reg [10:0] cnt;
+  reg [ 4:0] state;
+  reg [ 9:0] top;
+  reg [ 9:0] left;
+  reg [ 3:0] digit;
 
-    wire [3:0] grid_x, grid_y;
-    reg [3:0] digit_id;
-    wire [3:0] digit_floor, digit_health, digit_key_0,
-               digit_key_1, digit_key_2, digit_key_3;
+  render_digit u_render_digit (
+      .clk     (clk),
+      .rstn    (rstn),
+      .digit   (digit),
+      .top     (top),
+      .left    (left),
+      .dst_addr(dst_addr),
+      .dst_data(dst_data),
+      .dst_wr  (dst_wr)
+  );
 
-    get_digit #(.LEN(16))
-    u_get_digit_floor (
-        .number(floor),
-        .index(3),
-        .digit(digit_floor)
-    );
-    get_digit #(.LEN(16))
-    u_get_digit_health (
-        .number(health),
-        .index(grid_x),
-        .digit(digit_health)
-    );
-    get_digit #(.LEN(8))
-    u_get_digit_key_0 (
-        .number(key_num[7:0]),
-        .index(grid_x),
-        .digit(digit_key_0)
-    );
-    get_digit #(.LEN(8))
-    u_get_digit_key_1 (
-        .number(key_num[15:8]),
-        .index(grid_x),
-        .digit(digit_key_1)
-    );
-    get_digit #(.LEN(8))
-    u_get_digit_key_2 (
-        .number(key_num[23:16]),
-        .index(grid_x),
-        .digit(digit_key_2)
-    );
-    get_digit #(.LEN(8))
-    u_get_digit_key_3 (
-        .number(key_num[31:24]),
-        .index(grid_x),
-        .digit(digit_key_3)
-    );
-
-    always @(*) begin
-        case(grid_y)
-            0: digit_id <= digit_floor;
-            1: digit_id <= digit_health;
-            2: digit_id <= digit_key_0;
-            3: digit_id <= digit_key_1;
-            4: digit_id <= digit_key_2;
-            5: digit_id <= digit_key_3;
-            default: digit_id <= 0;
-        endcase
+  always @(posedge clk or negedge rstn) begin
+    if (!rstn) begin
+      cnt   <= 0;
+      state <= 0;
+    end else begin
+      cnt <= cnt + 1;
+      if (&cnt) begin
+        if (state < 20) state <= state + 1;
+        else state <= 0;
+      end
     end
+  end
 
-    render_number u_render_number (
-        .clk(clk),
-        .rstn(rstn),
-        .digit_id(digit_id),
-        .grid_x(grid_x),
-        .grid_y(grid_y),
-        .dst_addr(dst_addr),
-        .dst_data(dst_data),
-        .dst_wr(dst_wr)
-    );
+  reg [15:0] tmp;
+  always @(posedge clk or negedge rstn) begin
+    if (!rstn) begin
+      top  <= 0;
+      left <= 0;
+    end else begin
+      if (&cnt) begin
+        if (state == 20) begin
+          top   <= 86;
+          left  <= 58;
+          digit <= floor[3:0];
+        end else begin
+          if (state % 4 == 0) begin
+            if (state == 0) top <= 134;
+            else top <= top + 32;
+            left <= 98;
+            if (state == 0) begin
+              tmp   <= health / 10;
+              digit <= health % 10;
+            end else if (state == 4) begin
+              tmp   <= key_num[7:0] / 10;
+              digit <= key_num[7:0] % 10;
+            end else if (state == 8) begin
+              tmp   <= key_num[15:8] / 10;
+              digit <= key_num[15:8] % 10;
+            end else if (state == 12) begin
+              tmp   <= key_num[23:16] / 10;
+              digit <= key_num[23:16] % 10;
+            end else if (state == 16) begin
+              tmp   <= key_num[31:24] / 10;
+              digit <= key_num[31:24] % 10;
+            end
+          end else begin
+            left  <= left - 16;
+            tmp   <= tmp / 10;
+            digit <= tmp % 10;
+          end
+        end
+      end
+    end
+  end
+
+  /*
+    position:
+    20
+     3  2  1  0
+     7  6  5  4
+    11 10  9  8
+    15 14 13 12
+    19 18 17 16
+    */
 
 endmodule
